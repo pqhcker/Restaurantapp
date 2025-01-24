da2f3af8382eed3cfb35754232a9afd2fb971f27
Alejandro Solano <alejandro.solano@flypaperplane.com>
2025-01-24 15:07:38 -0600

    297. Navigation

--- App.tsx
+++ App.tsx
@@ -1,30 +1,57 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import Menu from './views/Menu';
 import NuevaOrden from './views/NuevaOrden';
+import Menu from './views/Menu';
 import DetallePedido from './views/DetallePlatillo';
 import FormularioPlatillo from './views/FormularioPlatillo';
-import ProgresoPedido from './views/ProgresoPedido';
 import ResumenPedido from './views/ResumenPedido';
+import ProgresoPedido from './views/ProgresoPedido';
 
 const Stack = createStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name={'Menu'} component={Menu} />
-        <Stack.Screen name={'NuevaOrden'} component={NuevaOrden} />
-        <Stack.Screen name={'DetallePedido'} component={DetallePedido} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {backgroundColor: '#FFDA00'},
+          headerTitleStyle: {fontWeight: 'bold'},
+          headerTintColor: '#000',
+        }}>
+        <Stack.Screen
+          name={'NuevaOrden'}
+          component={NuevaOrden}
+          options={{title: 'Nueva Orden'}}
+        />
+        <Stack.Screen
+          name={'Menu'}
+          component={Menu}
+          options={{title: 'Nuestro Menu'}}
+        />
+
+        <Stack.Screen
+          name={'DetallePedido'}
+          component={DetallePedido}
+          options={{title: 'Detalle Platillo'}}
+        />
         <Stack.Screen
           name={'FormularioPlatillo'}
           component={FormularioPlatillo}
+          options={{title: 'Ordenar Platillo'}}
         />
-        <Stack.Screen name={'ProgresoPedido'} component={ProgresoPedido} />
-        <Stack.Screen name={'ResumenPedido'} component={ResumenPedido} />
+        <Stack.Screen
+          name={'ResumenPedido'}
+          component={ResumenPedido}
+          options={{title: 'Resumen Pedido'}}
+        />
+        <Stack.Screen
+          name={'ProgresoPedido'}
+          component={ProgresoPedido}
+          options={{title: 'Progreso Pedido'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
